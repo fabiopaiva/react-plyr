3d43c78c9830f8d712437b82357490cbcfc6f83d
Jose Miguel Bejarano <yo@josemiguel.org>
2016-12-11 18:12:49 +0100

    📝 show component props in Storybook

--- stories/index.js
+++ stories/index.js
@@ -4,13 +4,13 @@ import { storiesOf } from '@kadira/storybook';
 import Plyr from '../src';
 
 storiesOf('React Plyr', module)
-  .add('default Youtube video', () => (
+  .addWithInfo('default Youtube video', () => (
     <Plyr
       type="youtube"
       videoId="CDFN1VatiJA"
     />
   ))
-  .add('default Vimeo video', () => (
+  .addWithInfo('default Vimeo video', () => (
     <Plyr
       type="vimeo"
       videoId="https://vimeo.com/189789787"
